0b4776d9fb20a232df12892b368a10502c12c841
umarabbas75 <umarabbas75@gmail.com>
2025-04-14 14:57:44 +0500

    build deleted

--- dist/src/notifiications/notification.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { NotificationService } from './notification.service';
-import { User } from '@prisma/client';
-export declare class NotificationController {
-    private readonly forumThreadService;
-    constructor(forumThreadService: NotificationService);
-    getUserNotifications(user: User): Promise<any>;
-    markNotificationAsRead(body: any): Promise<any>;
-}
--- dist/src/notifiications/notification.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class NotificationService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    getUserNotifications(userId: string): Promise<any>;
-    notifyAllUsersForNewThread(threadId: string, commenterId: string): Promise<void>;
-    markNotificationAsRead(notificationId: string): Promise<any>;
-}
